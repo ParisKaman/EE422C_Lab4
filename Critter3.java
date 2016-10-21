a26975249eb4b8524e3a73bbccaf6b862be3af07
ParisKaman <pariskaman@utexas.edu>
2016-10-20 19:46:34 -0500

    Update Critter3.java
    Header

--- Critter3.java
+++ Critter3.java
@@ -1,4 +1,13 @@
-/*
+/*	CRITTERS Critter.java
+ *	EE422C Project 4 submission by
+ *	Aaron Thomas North
+ *	atm2324
+ *	16465
+ *	Paris Kaman
+ *	pak679
+ *	16445
+ *	Slip days used: <0>
+ *	Fall 2016
  * 	Critter3 is a critter that does not reproduce
  * 	Critter3 has the following characteristics:
  * 		-never reproduces
